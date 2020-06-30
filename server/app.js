1bfbe251a8aa8a8c59ef598040834dd6860ecd4d
spek <spek@pop-os.localdomain>
2020-07-01 00:38:17 +0900

    initial commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on ${port}`));
