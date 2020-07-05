e708023a2430e23e193598c2d6855415cd168ba8
sp-devi <spneuro01@gmail.com>
2020-07-05 14:32:47 +0900

    added react components(User)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import ShowUser from './components/ShowUser';
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <Route exact path='/' component={ShowUser} />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/ShowUser.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import User from './CreateUser'
+
+class ShowUser extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {}
+        }
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:8082/api/user')
+            .then(res => {
+                this.setState({
+                    user: res.data
+                })
+            })
+            .catch(err => {
+                console.log('Error showing User');
+            })
+    };
+
+    render() {
+        const user = this.state.user;
+        console.log(user);
+
+        return (
+            <div>
+                <h2>{user.name}</h2>
+            </div>
+        )
+
+    };
+};
+
+export default ShowUser;
+
