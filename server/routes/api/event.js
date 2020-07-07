563a16695d28130863c10a5065b24dd3c54ad968
sp-devi <spneuro01@gmail.com>
2020-07-08 00:53:18 +0900

    remove unncessary comments

--- server/routes/api/event.js
+++ server/routes/api/event.js
@@ -7,8 +7,6 @@ const router = express.Router();
 const Event = require('../../models/Event');
 
 router.get('/id', function (req, res) {
-    // console.log(res);
-    // res.send('Home');
     Event.find()
         .then(books => res.json(books))
         .catch(err => res.status(404).json({
