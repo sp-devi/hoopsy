1a2ab4f873c5ef0c67a3a19bee0c70b3069be4dc
sp-devi <spneuro01@gmail.com>
2020-07-25 23:12:03 +0900

    added calendar component

---
+++ client/src/components/Calendar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import axios from 'axios';
+
+class Calendar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            cityName: '',
+            eventName: '',
+            eventAddress: '',
+            date: '',
+            time: {
+                start: '',
+                end: ''
+            }
+        };
+    }
+}
