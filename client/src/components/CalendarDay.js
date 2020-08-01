6097aa6fd32129e008d6611cc5589a7ea40929e5
sp-devi <spneuro01@gmail.com>
2020-08-01 23:14:10 +0900

    added calendar_day component

---
+++ client/src/components/CalendarDay.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import axios from 'axios';
+
+class CalendarDay extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <p>2</p>
+                </div>
+            </div>
+        )
+    }
+}
