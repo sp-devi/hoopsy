30770e78bf2aed50e108b67c4db367f9393355d9
sp-devi <spneuro01@gmail.com>
2020-08-02 23:04:57 +0900

    added calendarDay event route

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import FindEventByCity from './components/FindEventByCity';
 import SportEventList from './components/SportEventList';
 import CreateSportEvent from './components/CreateSportEvent';
 import CreatedEventStatus from './components/CreateSportEvent';
+import ShowEventForTheDate from './components/CalendarDay';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/show-events' component={SportEventList} />
         <Route path='/create-event' component={CreateSportEvent} />
         <Route path='/create-event/submit' component={CreatedEventStatus} />
+        <Route path='/show-event/day' component={ShowEventForTheDate} />
       </div>
     </Router>
   );
